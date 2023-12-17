5300c6085b7c89cdc6e520db421ecc118c2bc033
SebastianTorres12 <105263296+SebastianTorres12@users.noreply.github.com>
2023-12-17 15:06:09 -0500

    Solicitudes investigador, solo vale cambio en iconos

--- front/src/app/organismos/components/organismo-view/list-soltds-org/list-soltds-org.component.ts
+++ front/src/app/organismos/components/organismo-view/list-soltds-org/list-soltds-org.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Importa CommonModule
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'
 
 @Component({
   selector: 'app-list-soltds-org',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './list-soltds-org.component.html',
   styleUrl: './list-soltds-org.component.css'
 })
 export class ListSoltdsOrgComponent {
+  solicitudes = [
+    {
+      titulo: 'Proyecto A',
+      importe: '$1000',
+      fechaInicio: '2023-01-01',
+      fechaFin: '2023-02-01',
+      nombreGrupo: 'Grupo A',
+      horasSemana: '20',
+      convocatoria: 'Convocatoria 1',
+      estado: ''  // Campo para controlar el estado de aprobación/desaprobación
+    },
+    {
+      titulo: 'Proyecto B',
+      importe: '$1500',
+      fechaInicio: '2023-03-01',
+      fechaFin: '2023-04-01',
+      nombreGrupo: 'Líder B',
+      horasSemana: '25',
+      convocatoria: 'Convocatoria 2',
+      estado: ''  // Campo para controlar el estado de aprobación/desaprobación
+    },
+    // ... más solicitudes ...
+  ];
 
+  toggleAprobada(solicitud: any) {
+    solicitud.estado = solicitud.estado === 'aprobada' ? '' : 'aprobada';
+  }
+
+  toggleDesaprobada(solicitud: any) {
+    solicitud.estado = solicitud.estado === 'desaprobada' ? '' : 'desaprobada';
+  }
 }
